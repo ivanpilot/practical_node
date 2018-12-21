bf23f483ab63fe28cbb5b0151bf1da7eecb881c0
Ivan Pilot <ivan@localhost.localdomain>
2018-12-21 20:04:09 +0000

    added router/dealer pattern

---
+++ microservices/zmq-filter-rep-cluster.js
@@ -0,0 +1,40 @@
+'use strict';
+const cluster = require('cluster');
+const fs = require('fs');
+const zmq = require('zeromq');
+
+const numWorkers = require('os').cpus().length;
+
+if(cluster.isMaster) {
+    //Master process creates ROUTER and DEALER sockets and binds enpoints
+    const router = zmq.socket('router').bind('tcp://127.0.0.1:60401');
+    const dealer = zmq.socket('dealer').bind('ipc://filer-dealer.ipc');
+
+    //Fwd messages between the router and the dealer
+    router.on('message', (...frames) => dealer.send(frames));
+    dealer.on('message', (...frames) => router.send(frames));
+
+    //listen for workers that come online
+    cluster.on('online', worker => console.log(`Worker ${worker.process.pid} is online`));
+
+    //for a worker process for each CPU
+    for(let i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+} else {
+    //Worker process creates a REP socket and connect to the DEALER
+    const responder = zmq.socket('rep').connect('ipc://filer-dealer.ipc');
+
+    responder.on('message', data => {
+        const request = JSON.parse(data);
+        console.log(`${process.pid} received request for: ${request.path}`);
+
+        fs.readFile(request.path, (err, content) => {
+            responder.send(JSON.stringify({
+                content: content.toString(),
+                timestamp: Date.now(),
+                pid: process.pid
+            }))
+        });
+    });
+}
