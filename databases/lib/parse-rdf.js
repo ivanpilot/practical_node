0c496ed9775b2262da429ba87103e56617e2ee92
ivan <ivanpilot@gmail.com>
2018-12-22 21:02:04 +0000

    done converting rdf into json

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -7,6 +7,10 @@ module.exports = rdf => {
 
     book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
     book.title = $('dcterms\\:title').text();
+    book.authors = $('pgterms\\:agent pgterms\\:name')
+        .toArray().map(el => $(el).text());
+    book.subjects = $('[rdf\\:resource$="/LCSH"]').parent().find('rdf\\:value')
+        .toArray().map(el => $(el).text());
 
     return book;
 };
